673ce782aca23a970d6bdfeb0eb4e8209811530a
Amiusco <amifranciscocortez@hotmail.com>
2017-07-17 19:23:56 -0500

    Listo !!

--- BindingList/BindingList/Clases/FactoriaTelefono.vb
+++ BindingList/BindingList/Clases/FactoriaTelefono.vb
@@ -1,5 +1,29 @@
-﻿Public Class FactoriaTelefono
+﻿
+Imports System.ComponentModel
 
+Public Class FactoriaTelefono
 
+	Private Shared _telefonos As BindingList(Of CTelefono)
+
+	'Nuevo CTelefono
+	Public Shared Function CrearCTelefono(nom As String, tfn As Decimal) As CTelefono
+
+		Dim tfno As New CTelefono()
+		tfno.Nombre = nom
+		tfno.Telefono = tfn
+
+		Return tfno
+
+	End Function
+
+	Public Shared Function ObtenerColeccionCTelefono() As BindingList(Of CTelefono)
+
+		_telefonos = New BindingList(Of CTelefono)()
+		Dim rnd As New Random()
+		For i As Integer = 1 To 9
+			_telefonos.Add(CrearCTelefono("Persona " & i, rnd.Next(191000000, 191999999)))
+		Next
+		Return _telefonos
+	End Function
 
 End Class
--- BindingList/BindingList/Form1.vb
+++ BindingList/BindingList/Form1.vb
@@ -1,3 +1,53 @@
-﻿Public Class Form1
+﻿Imports System.ComponentModel
 
+Public Class Form1
+
+	Private colTfnos As BindingList(Of CTelefono)
+
+	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+		colTfnos = FactoriaTelefono.ObtenerColeccionCTelefono()
+		listTfnos.DataSource = colTfnos
+		listTfnos.DisplayMember = "Nombre"
+		ctTfnoSelec.DataBindings.Add("Text", colTfnos, "Telefono")
+
+	End Sub
+
+	Private Sub btAñadir_Click(sender As Object, e As EventArgs) Handles btAñadir.Click
+
+		Dim tef As Decimal = 0
+		If ctNombre.Text.Length <> 0 AndAlso ctTfno.Text.Length <> 0 AndAlso Decimal.TryParse(ctTfno.Text, tef) Then
+
+			colTfnos.Add(FactoriaTelefono.CrearCTelefono(ctNombre.Text, tef))
+
+		End If
+
+	End Sub
+
+	Private Sub btBorrar_Click(sender As Object, e As EventArgs) Handles btBorrar.Click
+
+		Dim pos As Integer = listTfnos.SelectedIndex
+		If pos < 0 Then Return
+		colTfnos.RemoveAt(pos)
+
+	End Sub
+
+	Private Sub btModificar_Click(sender As Object, e As EventArgs) Handles btModificar.Click
+
+		Dim pos As Integer = listTfnos.SelectedIndex
+
+		If ctNombre.Text.Length <> 0 Then
+
+			colTfnos(pos).Nombre = ctNombre.Text
+
+		End If
+
+		Dim tef As Decimal = 0
+		If ctTfno.Text.Length <> 0 AndAlso Decimal.TryParse(ctTfno.Text, tef) Then
+
+			colTfnos(pos).Telefono = tef
+
+		End If
+
+	End Sub
 End Class
